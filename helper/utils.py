f2507c60cbe8b9647f61fd7b06d84b2d8fc0fc3e
Billa008 <136236635+Billa008@users.noreply.github.com>
2023-08-12 20:42:24 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -8,7 +8,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 5.00) == 0 or current == total:        
+    if round(diff % 8.00) == 0 or current == total:        
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
