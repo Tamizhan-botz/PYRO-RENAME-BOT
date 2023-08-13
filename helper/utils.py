063488e846d6670c13d4eb13ade973de116777f3
Billa008 <136236635+Billa008@users.noreply.github.com>
2023-08-13 21:10:47 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -11,8 +11,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
     if round(diff % 5.00) == 0 or current == total:        
         percentage = current * 100 / total
         speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 3600
+        elapsed_time = round(diff) * 100
+        time_to_completion = round((total - current) / speed) * 360
         estimated_total_time = elapsed_time + time_to_completion
 
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
