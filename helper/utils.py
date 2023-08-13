1a25a958d3bc398f7226e829ec0454ca37f5f1cc
Billa008 <136236635+Billa008@users.noreply.github.com>
2023-08-13 11:59:23 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -40,12 +40,12 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
 def humanbytes(size):    
     if not size:
         return ""
-    power = 4**10
+    power = 2**10
     n = 0
     Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
         size /= power
-        n += 4
+        n += 2
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'ʙ'
 
 
